122ce3c9586a9abd2a7fca405b307072b2c388cd
Wanderer2419 <wargamer34567@gmail.com>
2026-09-20 10:11:56 -0500

    Develop the basic Reservation class

--- include/Reservation.h
+++ include/Reservation.h
@@ -1,1 +1,26 @@
 // Reservation header file
+// Resource header file
+#ifndef RESERVATION_H
+#define RESERVATION_H
+#include <string>
+#include <vector>
+using namespace std
+
+class Reservation {
+    private:
+        string Reservation_ID;
+        string Student_ID;
+        string Student_Name;
+        string Resource_ID;
+		string Reservation_Date
+    public:
+        // Constructor
+        Reservation(string RID, string SID, string name, string ResourceID, string date);
+
+        string getReservationID();
+		string getStudentID();
+        string getStudentName();
+        string getResourceID();
+        string getReservationDate();
+};
+#endif
