9ed43e690820ea201b43a878a022db64cd7dd03c
DylanAllen824 <dylanator824@gmail.com>
2026-09-20 22:48:33 -0500

    Add reservation management functions to header

--- include/ReservationManager.h
+++ include/ReservationManager.h
@@ -3,7 +3,13 @@
 #define RESERVATION_MANAGER_H
 #include <vector>
 #include <fstream>
+#include <string>
 #include "Resource.h"
+#include "Reservation.h"
+#include "LinkedList.h"
+#include "WaitingList.h"
+#include "CancellationHistory.h"
+using namespace std;
 
 
 
@@ -11,6 +17,15 @@ void loadResources();
 void displayAllResources();
 void SearchResources();
 
+void loadReservations(LinkedList& active, int& nextID);
+void createReservation(LinkedList& active, WaitingList& waiting, int& nextID);
+void cancelReservation(LinkedList& active, WaitingList& waiting,
+                       CancellationHistory& history, int& nextID);
+void undoCancellation(LinkedList& active, CancellationHistory& history);
+void sortResources();
+void generateReport(LinkedList& active, WaitingList& waiting, CancellationHistory& history);
 
+
+#endif
 #endif
 
